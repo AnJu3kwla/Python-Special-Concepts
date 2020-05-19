3644b0f08741fba582da32e4e57bf6c644d9be8a
AnJu3kwla <pulithaanjana@gmail.com>
2020-05-19 22:41:06 +0530

    Initial commit

---
+++ MonteCarlo.py
@@ -0,0 +1,24 @@
+import random
+ 
+def randomWalk2(n):
+    #"Return coordinates after 'n' blocks random walk"
+    x, y = 0, 0
+
+    for i in range(n):
+        (dx, dy) = random.choice([(0,1),(0,-1),(1,0),(-1,0)])
+        x = x + dx
+        y = y + dy
+    return (x, y)
+
+number_of_walks = 10000
+
+for walk_length in range(1,31):
+    no_transport  = 0 #number of walks 4 or fewer blocks from home
+    for i in range(number_of_walks):
+        (x, y) = randomWalk2(walk_length)
+        distance = abs(x) + abs(y)
+        if distance <= 4:
+            no_transport +=1
+    no_transport_percentage = float(no_transport) / number_of_walks
+    print("Walk size = ", walk_length, " / '%' of no transport = ", 100 * no_transport_percentage)
+
---
+++ RandomWalk.py
@@ -0,0 +1,23 @@
+import random
+
+def randomWalk(n): #a random walk with n number of blocks
+        #Return cordinates after "n" number of blocks
+
+    x = 0
+    y = 0
+
+    for i in range(n):
+        step = random.choice(['N','S','E','W'])
+        if step == 'N':
+            y = y + 1
+        elif step == 'S':
+            y = y - 1
+        elif step == 'E':
+            x = x + 1
+        else:
+            x = x - 1
+    return (x,y)
+
+for i in range(25):
+    walk = randomWalk(10)
+    print(walk, "Distance from the home :", abs(walk[0]) + abs(walk[1]))
